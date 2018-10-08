decab5700c008fff3aa1116873d18bce3016b460
dalbamm <oscar.yoon13@gmail.com>
2018-10-09 02:51:21 +0900

    Sign in part done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 // Imports for loading & configuring the in-memory web api 
 import { HttpClientModule } from '@angular/common/http';
--- src/app/signin/signin.component.ts
+++ src/app/signin/signin.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-signin',
   templateUrl: './signin.component.html',
   styleUrls: ['./signin.component.css']
 })
+
 export class SigninComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+	constructor(  ){}
+  ngOnInit( ) {}
+  check(){
+ 	var email = document.getElementById("email-input").value;
+ 	var pawd = document.getElementById("pw-input").value;
+	console.log(email);
+	console.log(pawd);
+	email==="swpp@snu.ac.kr" && pawd ==="iluvswpp" ? alert("success") : alert("Email or password is wrong")
   }
-
 }
