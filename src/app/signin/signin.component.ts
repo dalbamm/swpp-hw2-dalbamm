df83c9f960c38830c222595dd1cae665612a2911
dalbamm <oscar.yoon13@gmail.com>
2018-10-09 03:03:51 +0900

    revise minor error TS2339

--- src/app/signin/signin.component.ts
+++ src/app/signin/signin.component.ts
@@ -9,8 +9,8 @@ export class SigninComponent implements OnInit {
 	constructor(  ){}
   ngOnInit( ) {}
   check(){
- 	var email = document.getElementById("email-input").value;
- 	var pawd = document.getElementById("pw-input").value;
+ 	var email = (<HTMLInputElement>document.getElementById("email-input")).value;
+ 	var pawd = (<HTMLInputElement>document.getElementById("pw-input")).value;
 	console.log(email);
 	console.log(pawd);
 	email==="swpp@snu.ac.kr" && pawd ==="iluvswpp" ? alert("success") : alert("Email or password is wrong")
